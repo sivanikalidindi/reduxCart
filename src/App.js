0be34ee8dc91b9233dfe972c8c0318fdb3ca0a3a
sivanikalidindi <121473375+sivanikalidindi@users.noreply.github.com>
2023-04-19 18:31:45 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="container">
+     <h1>E-commerce Shopping Cart Application</h1>
+          <hr /> 
+          <div className='row'>
+            <div className='col-md-8'>
+              <Products />
+            </div>
+          </div>
     </div>
   );
 }
